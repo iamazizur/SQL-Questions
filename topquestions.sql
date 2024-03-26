1dd63c1291b96500d98687ca6baa717efe3947e2
iamazizur <86642492+iamazizur@users.noreply.github.com>
2024-03-26 12:41:50 +0530

    Update topquestions.sql

--- topquestions.sql
+++ topquestions.sql
@@ -1,1 +1,16 @@
 -- SELECT Nth Highest Salary
+
+DECLARE @num INT = 3;
+
+SELECT DISTINCT TOP (@num) salary
+FROM employee
+ORDER BY salary DESC
+
+SELECT MIN(salary)
+FROM 
+(
+	SELECT DISTINCT TOP (@num) salary
+	FROM employee
+	ORDER BY salary DESC
+) E2
+ 
